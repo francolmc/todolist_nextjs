8aa3b82029510e4db732f64594636ccfa368bee2
Franco Morales Castro <franco.morales@outlook.com>
2023-05-30 14:39:13 -0400

    🩹 fix interface name for TodoRepository

--- src/core/repositories/itodo.repository.ts
+++ src/core/repositories/itodo.repository.ts
@@ -1,6 +1,6 @@
 import TodoEntity from '@core/entities/todo.entity';
 
-export default interface IRolRepository {
+export default interface ITodoRepository {
     create(todo: TodoEntity): Promise<TodoEntity>;
     update(id: number, todo: TodoEntity): Promise<TodoEntity>;
     delete(id: number): Promise<TodoEntity>;
